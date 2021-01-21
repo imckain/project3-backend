0e6b64df5dbd8de04cabe616d745b9349d1ecc04
Ian McKain <ian@Ians-iMac.local>
2021-01-20 20:43:47 -0500

    working on CRUD

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,5 +1,3 @@
-const axios = require('axios');
-
 const Listing = require('../models/listing')
 const Favorite = require('../models/favorite');
 
@@ -7,71 +5,80 @@ const ObjectID = require('mongoose').Types.ObjectId
 
 module.exports = {
     getListings,
-    create,
-    edit: editListing,
-    delete: deleteOne,
-    show
+    // create,
+    // edit: editListing,
+    // delete: deleteOne,
+    // show
 }
+
+// async function getListings(req, res) {
+//     try {
+//         const data = await Listing.find( req.body )
+//         res.json(data);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
 
 function getListings(req, res) {
     Listing.find(function(err, listings) {
         if (!err) {
-            res.send(listings);
+            res.json(listings);
         } else {
             console.log(err);
         }
     });
 };
 
-function create(req, res) {
-    const newListing = new Listing({
-        photo: req.body.photo,
-        price: req.body.price,
-        sqft: req.body.sqft,
-        bed: req.body.bed,
-        bath: req.body.bath,
-    })
+// function create(req, res) {
+//     const newListing = new Listing({
+//         photo: req.body.photo,
+//         price: req.body.price,
+//         sqft: req.body.sqft,
+//         bed: req.body.bed,
+//         bath: req.body.bath,
+//     })
 
-    newListing.save((err, listings) => {
-        if(!err) res.send(listings)
-        else console.log(err);
-    })
-}
+//     newListing.save((err, listings) => {
+//         if(!err) res.send(listings)
+//         else console.log(err);
+//     })
+// }
 
-function show(req, res) {
-    const id = req.params.id;
-    Listing.findById(id, (err, listing) => {
-        res.json(listing);
-    })
-}
+// function show(req, res) {
+//     const id = req.params.id;
+//     Listing.findById(id, (err, listing) => {
+//         res.json(listing);
+//     })
+// }
 
-function editListing(req, res) {
-    if (!ObjectID.isValid(req.params.id))
-        return res.status(400).send('No Record');
+// function editListing(req, res) {
+//     if (!ObjectID.isValid(req.params.id))
+//         return res.status(400).send('No Record');
 
-    const updatedListing = {
-        photo: req.body.photo,
-        price: req.body.price,
-        sqft: req.body.sqft,
-        bed: req.body.bed,
-        bath: req.body.bath,
-    }
+//     const updatedListing = {
+//         photo: req.body.photo,
+//         price: req.body.price,
+//         sqft: req.body.sqft,
+//         bed: req.body.bed,
+//         bath: req.body.bath,
+//     }
 
-    Listing.findByIdAndUpdate(req.params.id, { $set: updatedListing }, {new: true}, (err, listings) => {
-        if (!err) res.send(listings)
-        else console.log('Error while updating');
-    });
-};
+//     Listing.findByIdAndUpdate(req.params.id, { $set: updatedListing }, {new: true}, (err, listings) => {
+//         if (!err) res.send(listings)
+//         else console.log('Error while updating');
+//     });
+// };
 
-function deleteOne(req, res) {
-    if (!ObjectID.isValid(req.params.id))
-        return res.status(400).send('No Record');
+// function deleteOne(req, res) {
+//     if (!ObjectID.isValid(req.params.id))
+//         return res.status(400).send('No Record');
     
-    Listing.findByIdAndRemove(req.params.id, (err, listings) => {
-        if (!err) res.send(listings)
-        else console.log('Error while deleting');
-    })
-}
+//     Listing.findByIdAndRemove(req.params.id, (err, listings) => {
+//         if (!err) res.send(listings)
+//         else console.log('Error while deleting');
+//     })
+// }
 
 // async function getListings(req, res) {
 //     try {
